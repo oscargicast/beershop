a90b49b223d2174347eab8ba9521d61aef61b8a3
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2024-08-04 10:15:44 -0500

    test: :test_tube: Add test for order items structure

--- backend/tests/integration/test_api.py
+++ backend/tests/integration/test_api.py
@@ -13,6 +13,23 @@ def test_get_order_success(order):
     assert len(response.json().get("rounds")) == len(order.rounds)
 
 
+def test_get_order_structure(order):
+    response = client.get("/api/v1/orders/order-1")
+    assert response.status_code == 200
+    data = response.json()
+    items = data["items"]
+    # Corona.
+    item_corona = items[0]
+    assert item_corona["name"] == "Corona"
+    assert item_corona["price_per_unit"] == 115
+    assert item_corona["total"] == 2
+    # Club Colombia.
+    item_corona = items[1]
+    assert item_corona["name"] == "Club Colombia"
+    assert item_corona["price_per_unit"] == 110
+    assert item_corona["total"] == 2
+
+
 def test_not_found_order():
     response = client.get("/api/v1/orders/invalid-order")
     assert response.status_code == 404
